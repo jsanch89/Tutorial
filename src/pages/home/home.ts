596df2ef2a76287c211e64c276124e63438b6b35
julian sanchez <futbol982009@hotmail.es>
2017-11-10 13:05:15 -0500

    Inicio 2

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { MapaPage } from '../mapa/mapa';
+import { ViajePage } from '../viaje/viaje';
 
 @Component({
   selector: 'page-home',

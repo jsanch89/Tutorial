35b54cf1bd7fcb0251b3a71868375421d487fd65
julian sanchez <futbol982009@hotmail.es>
2017-11-16 15:02:03 -0500

    vea

---
+++ src/pages/crear-obstaculo/crear-obstaculo.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { CrearObstaculoPage } from './crear-obstaculo';
+
+@NgModule({
+  declarations: [
+    CrearObstaculoPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(CrearObstaculoPage),
+  ],
+})
+export class CrearObstaculoPageModule {}
---
+++ src/pages/crear-obstaculo/crear-obstaculo.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { SingletonService } from '../../services/singleton/singleton';
+
+/**
+ * Generated class for the CrearObstaculoPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-crear-obstaculo',
+  templateUrl: 'crear-obstaculo.html',
+})
+export class CrearObstaculoPage {
+ ancho:number;
+ largo:number;
+ sala:any[][];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public singleton: SingletonService) {
+    this.sala = singleton.getMapaL();
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad CrearObstaculoPage');
+  }
+
+  obstacu( thing ){
+     console.log( this.sala[0][0] );
+    // this.singleton.cambioEstado( posi );
+  }
+  
+}
